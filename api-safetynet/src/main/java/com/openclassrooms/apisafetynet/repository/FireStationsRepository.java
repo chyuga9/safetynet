4d0555af661761f7d7bf8760ba8bdd20b981ba3a
Chyuga <Chyuga@DESKTOP-IDEC152>
2021-04-24 21:06:38 +0200

    Ajout méthode pour chercher les adresses en fonction de la station

--- api-safetynet/src/main/java/com/openclassrooms/apisafetynet/repository/FireStationsRepository.java
+++ api-safetynet/src/main/java/com/openclassrooms/apisafetynet/repository/FireStationsRepository.java
@@ -1,11 +1,21 @@
 package com.openclassrooms.apisafetynet.repository;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.openclassrooms.apisafetynet.constants.DBConstants;
 import com.openclassrooms.apisafetynet.model.FireStation;
 
 @Repository
 public interface FireStationsRepository extends CrudRepository<FireStation, String> {
-
+	@org.springframework.data.jpa.repository.Modifying
+	@org.springframework.data.jpa.repository.Query(value = DBConstants.TEST_A, nativeQuery = true)
+	//@Query(value="SELECT person.first_name, person.last_name, person.address, person.phone FROM person WHERE firestation.station = ?1  firestation.address = person.address")
+	List<FireStation> findAllByStationNumber(int station);
+	
 }
