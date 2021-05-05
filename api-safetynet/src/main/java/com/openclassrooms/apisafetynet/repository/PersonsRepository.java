d116175f571a694e4e153ddd114e3ca037b430db
Chyuga9 <ced91@DESKTOP-QU6CVON>
2021-05-05 12:01:24 +0200

    Ajout méthodes pour trouver par IdBd et par adresse

--- api-safetynet/src/main/java/com/openclassrooms/apisafetynet/repository/PersonsRepository.java
+++ api-safetynet/src/main/java/com/openclassrooms/apisafetynet/repository/PersonsRepository.java
@@ -1,12 +1,15 @@
 package com.openclassrooms.apisafetynet.repository;
 
 import java.util.List;
+import java.util.Optional;
 
-
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.apisafetynet.constants.DBConstants;
+import com.openclassrooms.apisafetynet.dto.ResponseDto;
+import com.openclassrooms.apisafetynet.model.MedicalRecord;
 import com.openclassrooms.apisafetynet.model.Person;
 
 @Repository
@@ -14,6 +17,15 @@ public interface PersonsRepository extends CrudRepository<Person, String> {
 	//@org.springframework.data.jpa.repository.Query(value = DBConstants.ADDRESSES_BY_STATION, nativeQuery = true)
 	//@Query(value="SELECT person.first_name, person.last_name, person.address, person.phone FROM person WHERE firestation.station = ?1  firestation.address = person.address")
 	//Iterable<Person> findAllByStationNumber(String station);
+	//@org.springframework.data.jpa.repository.Query(value = DBConstants.SEARCH_IN_MEDICAL_RECORDS, nativeQuery = true)
+	//String  findByIdDb(String IdDb);
+	@org.springframework.data.jpa.repository.Query(value = DBConstants.TEST_JOINT_TABLE, nativeQuery = true)
+	void createRowInJointTable(String mr_id, String idBd);
+	@org.springframework.data.jpa.repository.Query(value = DBConstants.TEST_B, nativeQuery = true)
+	//@Query(DBConstants.TEST_B)
+	Iterable<ResponseDto> test();
+	Optional<Person> findByIdDb(String idDb);
+	Iterable<Person> findByAddress(String address);
 	
 
 }
