8ce9d9038a8488b67c0f376938bdb9d29ee67f22
Chyuga9 <ced91@DESKTOP-QU6CVON>
2021-06-27 13:56:19 +0200

    Ajout de l'annotation enableswagger2

--- api-safetynet/src/main/java/com/openclassrooms/apisafetynet/ApiSafetynetApplication.java
+++ api-safetynet/src/main/java/com/openclassrooms/apisafetynet/ApiSafetynetApplication.java
@@ -5,7 +5,10 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ApiSafetynetApplication {
 	
 	private static final Logger logger = LogManager.getLogger(ApiSafetynetApplication.class);
