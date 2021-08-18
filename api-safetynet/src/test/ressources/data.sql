71d11284e6e04a9cce14dbfc645f4b2df4a1aae8
Cedric <cedric.campagna@gmail.com>
2021-08-18 09:52:50 +0200

    Nouveau formattage du fichier pour que toutes les données entrent

--- api-safetynet/src/test/ressources/data.sql
+++ api-safetynet/src/test/ressources/data.sql
@@ -1,7 +1,8 @@
 --Il ne faut pas mettre de double quotes ("") sinon il y a un problème avec les données
 
+
 INSERT INTO persons (first_name, last_name, id_db, address, city, zip, phone, email) VALUES
-		('John', 'Boyd', 'John_Boyd', '1509 Culver St', 'Culver', '97451', '841-874-6512', 'jaboyd@email.com'),
+		('John', 'Boyd', 'John_Boyd', '1509 Culver St', 'Culver', '97451', '841-874-6512', 'jaboyd@email.com') ,
         ('Jacob', 'Boyd', 'Jacob_Boyd','1509 Culver St', 'Culver', '97451', '841-874-6513', 'drk@email.com' ),
         ('Tenley', 'Boyd','Tenley_Boyd', '1509 Culver St', 'Culver', '97451', '841-874-6512', 'tenz@email.com') ,
         ('Roger', 'Boyd','Roger_Boyd', '1509 Culver St', 'Culver', '97451', '841-874-6512', 'jaboyd@email.com') ,
@@ -24,3 +25,51 @@ INSERT INTO persons (first_name, last_name, id_db, address, city, zip, phone, em
         ('Kendrik', 'Stelzer','Kendrick_Stelzer', '947 E. Rose Dr', 'Culver', '97451', '841-874-7784', 'bstel@email.com' ),
         ('Clive', 'Ferguson','Clive_Ferguson', '748 Townings Dr', 'Culver', '97451', '841-874-6741', 'clivfd@ymail.com' ),
         ('Eric', 'Cadigan', 'Eric_Cadigan','951 LoneTree Rd', 'Culver', '97451', '841-874-7458', 'gramps@email.com' );
+
+INSERT INTO firestations (address, station) VALUES
+		( '1509 Culver St', '3' ),
+        ( '29 15th St', '2' ),
+        ( '834 Binoc Ave', '3' ),
+        ( '644 Gershwin Cir', '1' ),
+        ( '748 Townings Dr', '3' ),
+        ( '112 Steppes Pl', '3' ),
+        ( '489 Manchester St', '4' ),
+        ( '892 Downing Ct', '2' ),
+        ( '908 73rd St', '1' ),
+        ( '112 Steppes Pl', '4' ),
+        ( '947 E. Rose Dr', '1' ),
+        ( '748 Townings Dr', '3' ),
+        ( '951 LoneTree Rd', '2' );
+        
+              		--UPDATE `persons` SET `firestations_id` = '1' WHERE (`address` = '1509 Culver St');
+              		
+              		
+              		
+   INSERT INTO medicalrecords (first_name, last_name, id_bd, birthdate, medications, allergies) VALUES
+  ( 'John', 'Boyd','John_Boyd', TO_DATE('03-06-1989','MM-dd-yyyy'), 'aznol:350mg, hydrapermazol:100mg', 'nillacilan' ),
+
+       ( 'Jacob', 'Boyd', 'Jacob_Boyd', TO_DATE('03-06-1989','MM-dd-yyyy'), 'pharmacol:5000mg, terazine:10mg, noznazol:250mg',''  ),
+        ( 'Tenley', 'Boyd','Tenley_Boyd', TO_DATE('02-18-2012','MM-dd-yyyy'),'','peanut' ),
+        ( 'Roger', 'Boyd','Roger_Boyd', TO_DATE('09-06-2017','MM-dd-yyyy'),'' ,''  ),
+      ( 'Felicia', 'Boyd','Felicia_Boyd',TO_DATE('01-08-1986','MM-dd-yyyy'), 'tetracyclaz:650mg', 'xilliathal' ),
+          ( 'Jonanathan', 'Marrack','Jonanathan_Marrack', TO_DATE('01-03-1989','MM-dd-yyyy'),'' ,'' ),
+        ( 'Tessa', 'Carman','Tessa_Carman', TO_DATE('02-18-2012','MM-dd-yyyy'),'' ,''  ),
+        ( 'Peter', 'Duncan','Peter_Duncan', TO_DATE('09-06-2000','MM-dd-yyyy'),'','shellfish' ),
+        ( 'Foster', 'Shepard','Foster_Shepard', TO_DATE('01-08-1980','MM-dd-yyyy'),'' ,''  ),
+        ( 'Tony', 'Cooper','Tony_Cooper', TO_DATE('03-06-1994','MM-dd-yyyy'), 'hydrapermazol:300mg, dodoxadin:30mg', 'shellfish' ),
+          ( 'Lily', 'Cooper','Lily_Cooper', TO_DATE('03-06-1994','MM-dd-yyyy') ,'' ,'' ),
+        ( 'Sophia', 'Zemicks','Sophia_Zemmicks', TO_DATE('03-06-1988','MM-dd-yyyy'), 'aznol:60mg, hydrapermazol:900mg, pharmacol:5000mg, terazine:500mg', 'peanut, shellfish, aznol' ),
+        ( 'Warren', 'Zemicks','Warren_Zemmicks', TO_DATE('03-06-1985','MM-dd-yyyy'),'' ,''  ),
+        ( 'Zach', 'Zemicks','Zach_Zemmicks', TO_DATE('03-06-2017','MM-dd-yyyy'),'' ,''  ),
+         ( 'Reginold', 'Walker','Reginold_Walker', TO_DATE('08-30-1979','MM-dd-yyyy'), 'thradox:700mg', 'illisoxian' ),
+        ( 'Jamie', 'Peters','Jamie_Peters', TO_DATE('03-06-1982','MM-dd-yyyy'),'' ,''  ),
+       ( 'Ron', 'Peters','Ron_Peters', TO_DATE('04-06-1965','MM-dd-yyyy'),'' ,'' ),
+        ( 'Allison', 'Boyd','Allison_Boyd', TO_DATE('03-15-1965','MM-dd-yyyy'), 'aznol:200mg', 'nillacilan' ),
+        ( 'Brian', 'Stelzer','Brian_Stezler', TO_DATE('12-06-1975','MM-dd-yyyy'), 'ibupurin:200mg, hydrapermazol:400mg', 'nillacilan' ),
+        ( 'Shawna', 'Stelzer','Shawna_Stezler', TO_DATE('07-08-1980','MM-dd-yyyy'),'' ,''  ),
+        ( 'Kendrik', 'Stelzer','Kendrick_Stezler', TO_DATE('03-06-2014','MM-dd-yyyy'), 'noxidian:100mg, pharmacol:2500mg' ,''  ),
+        ( 'Clive', 'Ferguson','Clive_Ferguson', TO_DATE('03-06-1994','MM-dd-yyyy'),'' ,''  ),
+        ( 'Eric', 'Cadigan','Eric_Cadigan', TO_DATE('08-06-1945','MM-dd-yyyy'), 'tradoxidine:400mg' ,''  );
+        
+        
+        
